9418690b2f25c2f0bfe3021afd99f533a19eb3a0
Сеня <akt2992469@gmail.com>
2026-03-31 12:34:18 +0700

    В мейне сделали путь до нашего папоротника и ща буим выращивать

---
+++ app/src/main/java/com/example/tsumapapp/ui/screens/DecisionTreeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.tsumapapp.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+@Composable
+fun DecisionTreeScreen(modifier: Modifier = Modifier) {
+
+}
